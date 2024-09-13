76c175d6885bce571835315e16fe8f2ace861478
Behnam <behnam.modi@outlook.com>
2024-09-13 23:14:48 +0000

    add k6 react

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js|jsx$/,
         loader: "babel-loader",
         // by default, it resolves `node_modules`
       },
